528bb12b29b0e2aca9f046c2d5e4160716898f71
Colby Frey <cfrey@protonmail.com>
2025-12-06 00:47:23 -0500

    add e

--- send.py
+++ send.py
@@ -7,7 +7,8 @@ class User:
         self.int_size_bits = int_size_bits
         self.bbs = BlumBlumShub(int_size_bits)
         self.p = 0
-        self.q = 0  
+        self.q = 0 
+        self.e = 65537  # common choice for e
         
     @property
     def n(self):
@@ -25,11 +26,16 @@ class User:
         print(f"p: {self.p}")
         print(f"q: {self.q}")
         print(f"n: {self.n}")
+        print(f"phi: {self.phi}")
+        
+    def print_public_key(self):
+        print(f"Public Key (n, e): ({self.n}, {self.e})")
+        
         
     
 keys = User(64)
 keys.generate_rsa_keys()
-keys.debug_print_keys()
+keys.print_public_key()
 
-c = rsa_a.encrypt_message(keys.n, 65537)  # using common e=65537
-print(f"ciphertext: {c}")
+#c = rsa_a.encrypt_messag   e(keys.n, 65537)  # using common e=65537
+#print(f"ciphertext: {c}")
